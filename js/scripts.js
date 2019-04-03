564e1a2618f08f7570cd505376da32a399c5336a
Jaime Takikawa and Mathew Akre <mathew@>
2019-04-03 11:47:02 -0700

    add update scripts.js and more content to business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,29 @@
 // business logic
+var vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+var userInput = $("#sentence").val();
+
+console.log(userInput);
+
+console.log(sentence);
 var pigLatin = function(sentence) {
-  
+  // userInput = userInput.slice("");
+if (userInput != vowels) {
+  // userInput = userInput.unshift();
+  sentence = sentence + "ay";
+}else {
+  return false;
+  }
 };
 
 // user interface logic
 $(document).ready(function() {
-  $
+  $("form#user").submit(function(event) {
+    event.preventDefault();
+    var sentence = $("input#sentence").val();
+    var result = pigLatin(sentence);
+    $("#result").text(result);
+
+    $("#result").show();
+
+  });
 });
